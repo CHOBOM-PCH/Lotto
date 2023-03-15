ccc2f51d5c41b852e6bf8dc35f1d82d2eb380728
iwojpch <iwojpch@gmail.com>
2023-03-15 09:53:27 +0900

    Add files via upload

---
+++ Lotto/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LottoNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 해제해야 하는지 여부입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenExcel = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnGenerateNumbers = new System.Windows.Forms.Button();
+            this.listBoxNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenExcel
+            // 
+            this.btnOpenExcel.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenExcel.Name = "btnOpenExcel";
+            this.btnOpenExcel.Size = new System.Drawing.Size(103, 23);
+            this.btnOpenExcel.TabIndex = 0;
+            this.btnOpenExcel.Text = "Open Excel File";
+            this.btnOpenExcel.UseVisualStyleBackColor = true;
+            this.btnOpenExcel.Click += new System.EventHandler(this.btnOpenExcel_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(121, 17);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(0, 12);
+            this.lblFilePath.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 51);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 12);
+            this.lblStatus.TabIndex = 2;
+            // 
+            // btnGenerateNumbers
+            // 
+            this.btnGenerateNumbers.Location = new System.Drawing.Point(12, 80);
+            this.btnGenerateNumbers.Name = "btnGenerateNumbers";
+            this.btnGenerateNumbers.Size = new System.Drawing.Size(117, 23);
+            this.btnGenerateNumbers.TabIndex = 3;
+            this.btnGenerateNumbers.Text = "Generate Numbers";
+            this.btnGenerateNumbers.UseVisualStyleBackColor = true;
+            this.btnGenerateNumbers.Click += new System.EventHandler(this.btnGenerateNumbers_Click);
+            // 
+            // listBoxNumbers
+            // 
+            this.listBoxNumbers.FormattingEnabled = true;
+            this.listBoxNumbers.ItemHeight = 12;
+            this.listBoxNumbers.Location = new System.Drawing.Point(135, 80);
+            this.listBoxNumbers.Name = "listBoxNumbers";
+            this.listBoxNumbers.Size = new System.Drawing.Size(295, 184);
+            this.listBoxNumbers.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 274);
+            this.Controls.Add(this.listBoxNumbers);
+            this.Controls.Add(this.btnGenerateNumbers);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnOpenExcel);
+            this.Name = "Form1";
+            this.Text = "Lotto Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenExcel;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnGenerateNumbers;
+        private System.Windows.Forms.ListBox listBoxNumbers;
+    }
+}
---
+++ Lotto/Form1.cs
@@ -0,0 +1,107 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LottoNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+        private Dictionary<int, int> numberFrequencies = new Dictionary<int, int>();
+        private List<int> numbers = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenExcel_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.Title = "Select an Excel file";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                lblFilePath.Text = filePath;
+
+                // Read numbers from Excel file
+                ReadNumbersFromExcel(filePath);
+            }
+        }
+
+        private void ReadNumbersFromExcel(string filePath)
+        {
+            // Clear existing numbers
+            numberFrequencies.Clear();
+            numbers.Clear();
+
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            int number = 0;
+                            if (int.TryParse(reader.GetValue(i).ToString(), out number))
+                            {
+                                if (!numberFrequencies.ContainsKey(number))
+                                {
+                                    numberFrequencies.Add(number, 0);
+                                }
+
+                                numberFrequencies[number]++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Calculate the total number of occurrences
+            int totalOccurrences = numberFrequencies.Values.Sum();
+
+            // Calculate the frequency of each number and add it to the list
+            foreach (int number in numberFrequencies.Keys)
+            {
+                int frequency = (int)((double)numberFrequencies[number] / totalOccurrences * 100);
+                for (int i = 0; i < frequency; i++)
+                {
+                    numbers.Add(number);
+                }
+            }
+        }
+
+        private void btnGenerateNumbers_Click(object sender, System.EventArgs e)
+        {
+            // Generate 5 sets of numbers
+            for (int i = 0; i < 5; i++)
+            {
+                List<int> set = new List<int>();
+                for (int j = 0; j < 7; j++)
+                {
+                    // Choose a random number from the list
+                    int index = new Random().Next(0, numbers.Count);
+                    int number = numbers[index];
+
+                    // Add the number to the set and remove it from the list
+                    set.Add(number);
+                    numbers.RemoveAt(index);
+                }
+
+                // Sort the set and add it to the list box
+                set.Sort();
+                listBoxNumbers.Items.Add(string.Join(", ", set));
+            }
+
+            // Update the status label
+            lblStatus.Text = "Numbers generated.";
+        }
+    }
+}
